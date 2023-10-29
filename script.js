cbd50b1a765ba7c171da2a4a87bbceb6e5c4b891
ACodingSunflower <125006517+ACodingSunflower@users.noreply.github.com>
2023-10-29 22:46:43 +0200

    made menu solid colour

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const toggleNav = () => {
     const currentDisplay = document.getElementById('burgerMenu').style.display;
     if(currentDisplay === "block") {
-        document.getElementById('burgerMenu').style.display = "none"
+        document.getElementById('burgerMenu').style.display = "none";
+        //display lines
+        document.getElementById('closedMenu').src = "./images/burger.png";
+
     } else {
-        document.getElementById('burgerMenu').style.display = "block"
+        document.getElementById('burgerMenu').style.display = "block";
+        //display cross
+        document.getElementById('closedMenu').src = "./images/x.png";
     }
 }
 
 const openNav = () => {
-    document.getElementById('burgerMenu').style.display = 'block';
-  }
+    document.getElementById('burgerMenu').style.display = "block";
+}
   
   const closeNav = () => {
     document.getElementById("burgerMenu").style.display = "none";
-  }
+}
